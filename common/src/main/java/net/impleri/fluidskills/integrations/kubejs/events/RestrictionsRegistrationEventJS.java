2d44ee491f1d9bee31954ada6aa3b7c484a08165
Christopher Roussel <christopher@impleri.net>
2023-04-20 22:30:16 -0500

    fix: Update imports from PlayerSkills

--- common/src/main/java/net/impleri/fluidskills/integrations/kubejs/events/RestrictionsRegistrationEventJS.java
+++ common/src/main/java/net/impleri/fluidskills/integrations/kubejs/events/RestrictionsRegistrationEventJS.java
@@ -5,7 +5,7 @@ import dev.latvian.mods.rhino.util.HideFromJS;
 import net.impleri.fluidskills.FluidHelper;
 import net.impleri.fluidskills.FluidSkills;
 import net.impleri.fluidskills.restrictions.Restriction;
-import net.impleri.playerskills.restrictions.AbstractRegistrationEventJS;
+import net.impleri.playerskills.integration.kubejs.api.AbstractRegistrationEventJS;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
